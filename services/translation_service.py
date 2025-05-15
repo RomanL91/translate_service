1f734866567c4ac770a8d5a8ccb19511dd594db0
RomanL91 <dev_roman91@mail.ru>
2025-05-15 17:35:18 +0500

    раздача конфигов

--- services/translation_service.py
+++ services/translation_service.py
@@ -1,6 +1,8 @@
 from services.rabbitmq_repository import RabbitMQRepository
 from services.translator_service import TranslatorService
 
+from app import settings
+
 
 class TranslationService:
     """Основной сервис для управления переводами"""
@@ -11,10 +13,13 @@ class TranslationService:
         self.rabbitmq_repo = rabbitmq_repo
         self.translator_service = translator_service
 
+        # имя очереди для переведенного
+        self.response_queue = settings.response_translation_queue
+
     async def send_translation_request(self, text: str, target_lang: str):
         """Отправляет запрос на перевод в очередь"""
         message = {"text": text, "target_lang": target_lang}
-        await self.rabbitmq_repo.publish_message("translation_queue", message)
+        await self.rabbitmq_repo.publish_message(settings.translation_queue, message)
 
     async def process_translation(self, payload: dict):
         """Обрабатывает сообщение из очереди"""
@@ -29,8 +34,15 @@ class TranslationService:
         translated_text = await self.translator_service.translate(text, target_lang)
         print(f"✅ Перевод: '{text}' -> '{translated_text}'")
 
+        # Формируем новое сообщение с переводом
+        new_message = payload.copy()
+        new_message["text"] = translated_text
+
+        # Публикуем перевод в очередь response_queue
+        await self.rabbitmq_repo.publish_message(self.response_queue, new_message)
+
     async def start_consumer(self):
         """Запуск прослушивания очереди"""
         await self.rabbitmq_repo.consume_messages(
-            "translation_queue", self.process_translation
+            settings.translation_queue, self.process_translation
         )
